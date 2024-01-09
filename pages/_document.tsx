b1901b600c26f7f01a234b201d06b73631ddeb23
mtluc <tienluc0811@gmail.com>
2024-01-09 13:39:29 +0700

    test dynamic thumbai

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,41 +10,10 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1"
         />
-        <meta charSet="UTF-8" />
-        <meta property="og:locale" content="vi_VN" />
-        <meta name="robots" content="index,follow,noodp" />
-        <meta name="robots" content="noarchive" />
-        <meta httpEquiv="Content-Language" content="vi" />
-        <meta name="robots" content="max-image-preview:large" />
-        <meta name="Language" content="vi" />
-
-        <meta property="og:title" itemProp="name" content="wedding" />
-
-        <meta name="description" content="wedding" />
-
-        <meta property="og:description" content="wedding" />
-
-        <meta
-          content={`/wedding-thumbai/${encodeURIComponent('Lực 123')}`}
-          property="og:image"
-          itemProp="thumbnailUrl"
-        />
-
-        <meta name="keywords" content="wedding|wedding online" />
-        
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto"
+          href={`https://fonts.googleapis.com/css?family=Roboto|${fonts.join('|')}`}
         />
-        {
-          fonts.map((font) => {
-            return <link
-              key={font}
-              rel="stylesheet"
-              href={`https://fonts.googleapis.com/css?family=${font}`}
-            />
-          })
-        }
       </Head>
       <body>
         <Main />
