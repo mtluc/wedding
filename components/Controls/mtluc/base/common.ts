ebc74580ff19662c68b3752ba6a9df5b51448487
mtluc <tienluc0811@gmail.com>
2024-07-09 13:30:49 +0700

    sửa lỗi xóa tiếng việt

--- components/Controls/mtluc/base/common.ts
+++ components/Controls/mtluc/base/common.ts
@@ -345,7 +345,11 @@ export const fireMouseDown = (el: HTMLElement | Document = document) => {
 };
 
 export const removeVietnameseTones = (text: string) => {
-  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D");
 };
 
 export const getQueryUrl = () => {
